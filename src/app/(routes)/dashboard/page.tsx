eef113fabad799c6f678633e7a4a063533cc4a06
Abhinav Kumar <abhinavkr.2108@gmail.com>
2024-03-15 11:15:31 +0530

    Create User in Convex Database. React strict mode set to false to avoid multiple entries of a single user

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import Header from "@/components/shared/Header";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useQuery } from "convex/react";
 import React from "react";
+import { api } from "../../../../convex/_generated/api";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { getUser } = getKindeServerSession();
+  const currentUser = await getUser();
+
+  if (!currentUser || !currentUser.email) {
+    return "Not logged in";
+  }
+
   return (
     <div>
-      <Header />
+      <Header user={currentUser} />
     </div>
   );
 }
